c780efc3785a8f09559aabdc3017411b29ac45f6
Flávio Henrique <henriqueflavio55@gmail.com>
2023-12-28 18:49:54 -0300

    edit location of favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Favicon from '/public/favicon.ico';
 import { Sidebar } from '@/components/Sidebar';
 import type { Metadata } from 'next';
 import { Barlow } from 'next/font/google';
@@ -12,6 +13,7 @@ export const metadata: Metadata = {
   title: 'Delicious Dishes',
   description:
     'Explore a world of culinary delights with Delicious Dishes, a Next.js app dedicated to bringing you mouthwatering recipes and cooking inspiration. Discover a diverse range of dishes, from appetizers to desserts, with step-by-step instructions and stunning visuals.',
+  icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({
